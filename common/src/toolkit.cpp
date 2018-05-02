cb5b1cc854cb44b0efc4ad90e6679959cedfcf6b
Azael Reyes <azael.rmartz@gmail.com>
2018-05-01 21:20:09 -0700

    Se agrego el atributo stage a la clase Version y funcion toString a la misma clase.

--- common/src/toolkit.cpp
+++ common/src/toolkit.cpp
@@ -4,35 +4,91 @@
 
 namespace toolkit
 {
+	
+	Version::Stage Version::getStage()
+	{
+		return stage;
+	}	
+	Version::Version(const Version &version)
+	{
+		this->major = version.major;
+		this->minor = version.minor;
+		this->patch = version.patch;
+		this->stage = version.stage;
+	}
+	std::string Version::toString()
+	{
+		std::string ver = "";
+		if(major > -1)
+		{
+			ver += std::to_string(major);
+		}		
+		if(minor > -1)
+		{
+			if(major > -1)
+			{
+				ver += ".";
+			}
+			ver += std::to_string(minor);
+		}
+		if(patch > -1)
+		{
+			if(major > -1 || minor > -1)
+			{
+				ver += ".";
+			}
+			ver += std::to_string(patch);
+		}
+		if(stage == alpha)
+		{
+			ver += "-alpha";
+		}
+		else if(stage == beta)
+		{
+			ver += "-beta";
+		}
+		else if(stage == release)
+		{
+			ver += "-release";
+		}
+		
+		return ver.c_str();
+	}
 	Version::Version()
 	{
 		this->major = -1;
 		this->minor = -1;
 		this->patch = -1;
+		this->stage = unknown;
 	}
 	Version::Version(short major)
 	{
 		this->major = major;
 		this->minor = -1;
 		this->patch = -1;
+		this->stage = unknown;
 	}
 	Version::Version(short major,short minor)
 	{
 		this->major = major;
 		this->minor = minor;
 		this->patch = -1;
+		this->stage = unknown;
 	}
 	Version::Version(short major,short minor,short patch)
 	{
 		this->major = major;
 		this->minor = minor;
 		this->patch = patch;
+		this->stage = unknown;
 	}
-	Version getVersionCommon()
+	Version::Version(short major,short minor,short patch,Stage stage)
 	{
-		return Version(MAJOR,MINOR,PATCH);		
-	}
-	
+		this->major = major;
+		this->minor = minor;
+		this->patch = patch;
+		this->stage = stage;
+	}	
 	short Version::getMajor()
 	{
 		return major;
@@ -44,7 +100,31 @@ namespace toolkit
 	short Version::getPatch()
 	{
 		return patch;
+	}		
+	Version::Version(short major,Stage stage)
+	{
+		this->major = major;
+		this->minor = -1;
+		this->patch = -1;
+		this->stage = stage;
 	}
+	Version::Version(short major,short minor,Stage stage)
+	{
+		this->major = major;
+		this->minor = minor;
+		this->patch = -1;
+		this->stage = stage;
+	}
+	
+	
+		
+	Version getVersionCommon()
+	{
+		return Version(MAJOR,MINOR,PATCH,STAGE);		
+	}
+	
+	
+	
 	
     Exception::Exception(Code code,const char* description) throw() : Message(code,description)
     {
@@ -69,11 +149,6 @@ namespace toolkit
         if(code < 0) return true;
         return false;
     }
-    /*Message::Message()
-    {
-        code = NOTHINK;
-        description = NULL;
-    }*/
     Message::Message(Code code,const char* description)
     {
         this->code = code;
--- common/src/toolkit.hpp
+++ common/src/toolkit.hpp
@@ -6,9 +6,56 @@
 
 namespace toolkit
 {
+	/**
+	 * \brief Get version info
+	 * 
+	 **/
+	class Version
+	{
+	public:
+		enum Stage
+		{
+			unknown,
+			alpha,
+			beta,
+			release
+		};
+		Version();
+		Version(short major);
+		Version(short major,short minor);
+		Version(short major,short minor,short patch);
+		Version(short major,short minor,short patch,Stage stage);
+		Version(short major,Stage stage);
+		Version(short major,short minor,Stage stage);
+		Version(const Version &version);
+		short getMajor();
+		short getMinor();
+		short getPatch();
+		Stage getStage();
+		std::string toString();	
+		
+	private:
+		short major;
+		short minor;
+		short patch;
+		Stage stage;	
+	};
+	
+	/**
+	 * \brief return tha actual version of common component
+	 **/
+	Version getVersionCommon();
+	
+	/**
+	 * \brief Gets infor of status and description of operation
+	 * 
+	 **/
     class Message
     {
     public:
+		/**
+		 * \brief Status code
+		 **/
         enum Code
         {
             FAIL_SRD_CONNECTION     =   -1001,
@@ -18,8 +65,14 @@ namespace toolkit
             SUCCEED =   1,
             SUCCEED_SERVER_DATABASE =   1000,
         };
-        bool isPass();
-        bool isFail();
+        /**
+         * \return true if code is greater than zero
+         **/
+        bool isPass();        
+        /**
+         * \return true if code is smaller than zero
+         **/
+        bool isFail();        
         //Message();
         Message(Code, const char* description);
         virtual const char* what() const throw();
@@ -28,14 +81,21 @@ namespace toolkit
         const char* description;
         Code code;
     };
-
+	
+	/**
+	 * \brief A Message qith status corect.
+	 **/
     class Confirmation : public Message
     {
     public:
         Confirmation(Code number, const char* description) throw();
         //Confirmation() throw();
     };
-
+	
+	/**
+	 * \brief Implmetation of std::exception with support for Message,
+	 *        it mean the process fail.
+	 */
     class Exception : public Message, public std::exception
     {
     public:
@@ -46,25 +106,8 @@ namespace toolkit
         virtual const char* what() const throw();
         Exception(Code number,const char* description) throw();
         //Exception()throw();
-    };
-
-	class Version
-	{
-	private:
-		short major;
-		short minor;
-		short patch;
-	public:
-		Version();
-		Version(short major);
-		Version(short major,short minor);
-		Version(short major,short minor,short patch);
-		short getMajor();
-		short getMinor();
-		short getPatch();		
-	};
+    };	
 	
-	Version getVersionCommon();	
 }
 
 #endif
