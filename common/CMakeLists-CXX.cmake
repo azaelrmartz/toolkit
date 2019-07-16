0c47f26d91dba93202e11b858423ff1f002a749c
Azael R <azael.rmartz@gmail.com>
2019-07-15 17:25:33 -0700

    Reconfiguracion de projecto
    Ahora se pueden tener diferentes versiones para cada projecto.

--- common/CMakeLists-CXX.cmake
+++ common/CMakeLists-CXX.cmake
@@ -1,10 +1,14 @@
+PROJECT(octetos-toolkit-common-c++ VERSION 4.0.0.1 LANGUAGES ${LANG})
 
+EXECUTE_PROCESS(COMMAND date +"%Y%m%d%H%M%S" OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_BUILD)
+SET(${PROJECT_NAME}_VERSION_STAGE "snapshot")
+SET(${PROJECT_NAME}_VERSION_NAME "reader")
 SET(CMAKE_CXX_STANDARD 11)
 SET(CMAKE_CXX_STANDARD_REQUIRED ON)
 SET(CMAKE_CXX_EXTENSIONS OFF)
-
 ENABLE_TESTING()
 CONFIGURE_FILE("${PROJECT_SOURCE_DIR}/versionInfo.h.in" "${PROJECT_SOURCE_DIR}/versionInfo.h")
+
 
 FIND_PACKAGE(CUnit REQUIRED PATHS ${PROJECT_SOURCE_DIR}/../cmake/Modules/)
 IF(CUNIT_FOUND)
@@ -19,11 +23,11 @@ ADD_FLEX_BISON_DEPENDENCY(lexer parser)
 
 #ADD_EXECUTABLE(main main.cpp common.cpp) 
 #ADD_EXECUTABLE(parserVersionTest common.cpp parserVersion-C++/version-c++.cc parserVersion-C++/driver.cc ${FLEX_lexer_OUTPUTS} ${BISON_parser_OUTPUTS})
-ADD_EXECUTABLE(testing-v${octetos-toolkit-common_VERSION_MAJOR} tests/v${octetos-toolkit-common_VERSION_MAJOR}.cc common.cpp parserVersion-C++/driver.cc ${FLEX_lexer_OUTPUTS} ${BISON_parser_OUTPUTS})
-TARGET_LINK_LIBRARIES(testing-v${octetos-toolkit-common_VERSION_MAJOR} ${CUNIT_LIBRARY})
+ADD_EXECUTABLE(testing-v${octetos-toolkit-common-c++_VERSION_MAJOR} tests/v${octetos-toolkit-common-c++_VERSION_MAJOR}.cc common.cpp parserVersion-C++/driver.cc ${FLEX_lexer_OUTPUTS} ${BISON_parser_OUTPUTS})
+TARGET_LINK_LIBRARIES(testing-v${octetos-toolkit-common-c++_VERSION_MAJOR} ${CUNIT_LIBRARY})
 
-ADD_LIBRARY(${PROJECT_NAME}-c++ STATIC common-parser.cpp common.cpp parserVersion-C++/driver.cc ${FLEX_lexer_OUTPUTS} ${BISON_parser_OUTPUTS})
+ADD_LIBRARY(${PROJECT_NAME}  STATIC common-parser.cpp common.cpp parserVersion-C++/driver.cc ${FLEX_lexer_OUTPUTS} ${BISON_parser_OUTPUTS})
 
-INSTALL(TARGETS ${PROJECT_NAME}-c++ DESTINATION lib/octetos/toolkit/common/)
+INSTALL(TARGETS ${PROJECT_NAME} DESTINATION lib/octetos/toolkit/common/)
 INSTALL(FILES common.hpp DESTINATION include/octetos/toolkit/common/)
-SET(CPACK_PACKAGE_NAME "${PROJECT_NAME}-c++")
+SET(CPACK_PACKAGE_NAME "${PROJECT_NAME}")
